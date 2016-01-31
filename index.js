6f8d566dc3b52e87c944404fcff940ea85f5b601
Oleh Daybov <olegtranslator@gmail.com>
2016-01-31 17:00:45 +0200

    finished

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var x = [1, 5, 3, 6, 7, 4, 6, 3, 8, 4, 1, 7];
 
-for(i = 0; i < x.length; i++) {
-  console.log(x[i]);
+// Search numers which has doubles
+function searchDoubles(arr) {
+  for(i = 0; i < x.length; i++) {
+    console.log("x[" + i + "] = " + x[i]);
+    for(j = i; j >= 0; j--) {
+      if (x[i] = x[j]) {
+        console.log(x[i] + " has double");
+        break;
+      };
+    };
+  };
 };
+searchDoubles(x);
